e787056246dd027875de222a28a4f325b3535faf
Brad Davies <varbrad@users.noreply.github.com>
2018-01-14 15:55:17 +0000

    Initial setup for symlinks to player-app

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-module.exports = {
-  doThing() {
-    return 123;
+class Game {
+  /**
+   * @param {HTMLCanvasElement} canvas
+   */
+  constructor(canvas, width, height) {
+    this.canvas = canvas;
+    this.gc = this.canvas.getContext("2d");
+
+    // Set canvas width/height
+    this.canvas.width = width;
+    this.canvas.height = height;
+
+    this.reset();
   }
-};
+
+  /**
+   * Resets the Game as if it was never played before
+   */
+  reset() {}
+
+  /**
+   * Begins the game loop
+   */
+  start() {}
+
+  /**
+   * Pauses the game
+   */
+  pause() {
+    this.paused = true;
+  }
+
+  /**
+   * Un-pauses the game
+   */
+  resume() {
+    this.paused = false;
+  }
+}
+
+export default Game;
